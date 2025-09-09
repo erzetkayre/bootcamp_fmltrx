9602948e549c6ef65d93c529f7e04f3bf5cc104e
erzetkayre <erzekayre@gmail.com>
2025-09-09 13:54:55 +0700

    Logic Exercise New Commit

--- LogicExercise/Program.cs
+++ LogicExercise/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Logic Exercise Project
+using System.Runtime.CompilerServices;
+
+const int value = 30;
+
+for (int i = 1; i <= value; i++)
+{
+    if ((i % 3) == 0)
+        if ((i % 5) == 0)
+            Console.WriteLine("foobar");
+        else
+            Console.WriteLine("foo");
+    else if ((i % 5) == 0)
+        if ((i % 3) == 0)
+            Console.WriteLine("foobar");
+        else
+            Console.WriteLine("bar");
+    else
+        Console.WriteLine(i);
+}
+
+
