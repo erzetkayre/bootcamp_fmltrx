eae19aaca399b46cab6dca817cd774e818a4b836
erzetkayre <erzetkayre@gmail.com>
2025-09-23 13:23:29 +0700

    Logic Exercise 3

---
+++ LogicExercise3/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Input a number: ");
+
+int number;
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.WriteLine("Only Number Input...");
+}
+
+var words = new Dictionary<int, string>
+{
+    {3, "foo"},
+    {5, "bar"},
+    {7, "jazz"},
+    {9, "huzz"},
+};
+
+for (int i = 1; i <= number; i++)
+{
+    string result = "";
+    foreach (var word in words)
+    {
+        if (i % word.Key == 0)
+        {
+            result += word.Value;
+        }
+    }
+    Console.Write(string.IsNullOrEmpty(result) ? i + "  " : result + "  ");
+}
